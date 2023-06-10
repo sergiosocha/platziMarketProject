66f3d519bd323884b0c2e7c158fef0994f08d9e7
SergioPc <sergio_socha@yahoo.com>
2023-06-09 21:37:59 -0500

    new files

--- platzi-market/src/main/java/com/platzi/market/persistence/entity/Categoria.java
+++ platzi-market/src/main/java/com/platzi/market/persistence/entity/Categoria.java
@@ -1,2 +1,40 @@
-package com.platzi.market.persistence.entity;public class Categoria {
+package com.platzi.market.persistence.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_categoria")
+    private String idCategoria;
+
+    private String descripcion;
+
+    private Boolean estado;
+
+    public String getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(String idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
 }
--- platzi-market/src/main/java/com/platzi/market/persistence/entity/Cliente.java
+++ platzi-market/src/main/java/com/platzi/market/persistence/entity/Cliente.java
@@ -1,2 +1,64 @@
-package com.platzi.market.persistence.entity;public class Cliente {
+package com.platzi.market.persistence.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+    @Id
+    private String id;
+    private String name;
+    private String apellidos;
+    private Integer celular;
+    private String direccion;
+    @Column(name = "correo_electronico")
+    private String correoElectronico;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public Integer getCelular() {
+        return celular;
+    }
+
+    public void setCelular(Integer celular) {
+        this.celular = celular;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
 }
